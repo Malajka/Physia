955e20488ab94b0475889c70ae5d23c09995504a
Malajka <m.bieniecka@gmail.com>
2025-06-04 13:34:19 +0200

    fix: imports fixes

---
+++ src/components/ui/types.d.ts
@@ -0,0 +1,41 @@
+// TypeScript module declarations for UI components
+declare module "./Button" {
+  export const Button: any;
+  export const buttonVariants: any;
+}
+
+declare module "./Skeleton" {
+  export const Skeleton: any;
+}
+
+declare module "./Slider" {
+  export const Slider: any;
+}
+
+declare module "./Spinner" {
+  export const Spinner: any;
+}
+
+declare module "./ErrorAlert" {
+  export const ErrorAlert: any;
+}
+
+declare module "./Modal" {
+  export const Modal: any;
+}
+
+declare module "./LinkButton" {
+  export const LinkButton: any;
+}
+
+declare module "./InputField" {
+  export const InputField: any;
+}
+
+declare module "./InfoBar" {
+  export const InfoBar: any;
+}
+
+declare module "./SubmitButton" {
+  export const SubmitButton: any;
+}
