19c93784c9064a7e1b74fcd3671e43b02f1d3752
Malajka <m.bieniecka@gmail.com>
2025-06-04 13:30:34 +0200

    fix: imports fixes

--- src/components/ui/SubmitButton.tsx
+++ src/components/ui/SubmitButton.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Button } from "./Button";
 
-interface SubmitButtonProps extends React.ComponentProps<typeof Button> {
+interface SubmitButtonProps {
   loading?: boolean;
   children: React.ReactNode;
+  className?: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  type?: "button" | "submit" | "reset";
+  "data-testid"?: string;
 }
 
 export const SubmitButton = React.memo(function SubmitButton({ loading = false, className, children, ...props }: SubmitButtonProps) {
