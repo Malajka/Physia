0d7b4beff175bbd15fc1bf26cacc6ac381f7a99c
Malajka <m.bieniecka@gmail.com>
2025-06-04 13:12:56 +0200

    fix: eslint fixes

--- src/components/ui/SubmitButton.tsx
+++ src/components/ui/SubmitButton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "./Button";
+import { Button } from "@/components/ui/Button";
 
 interface SubmitButtonProps extends React.ComponentProps<typeof Button> {
   loading?: boolean;
