8225e9baad48c5df7447751098f0dddbb652a1cb
Malajka <m.bieniecka@gmail.com>
2025-06-09 21:03:24 +0200

    feat: debug file

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,8 +1,7 @@
-import { getSupabaseClient } from "@/db/supabase.client";
 import type { MiddlewareHandler } from "astro";
-import { handleRequest } from "./middlewareHandler";
 
 export const onRequest: MiddlewareHandler = async (context, next) => {
-  context.locals.supabase = getSupabaseClient();
-  return handleRequest(context, next);
+  // Skip Supabase initialization for now to test
+  console.log("Middleware running for:", context.url.pathname);
+  return next();
 };
