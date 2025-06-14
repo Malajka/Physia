4f2b9d938515839da9643b44b0c6658b028a5760
Malajka <m.bieniecka@gmail.com>
2025-06-14 09:48:55 +0200

    feat: add env-test API route to expose environment variables for debugging

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config";
-import path from "path";
-import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
+import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
