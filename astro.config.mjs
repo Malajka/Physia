f204a2e962c425329b968fe0e88c34c072c3a777
Malajka <m.bieniecka@gmail.com>
2025-06-15 08:35:50 +0200

    chore: update hosting provider from DigitalOcean to Vercel in tech stack documentation; minor cleanup in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,9 @@ import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 import path from "path";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel({}),
-  // Dodaj tailwind() do integracji
   integrations: [react(), sitemap(), tailwind()],
   server: { port: 4321 },
 
