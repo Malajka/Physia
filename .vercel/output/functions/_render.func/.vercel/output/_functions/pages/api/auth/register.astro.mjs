7e8568eaf57c0a10eb1fbeb6949bc98d33b0d122
Monika Bieniecka <m.bieniecka@gmail.com>
2025-08-04 16:01:21 +0200

    chore: update dependencies and remove unused Vercel output files
    - Upgraded @astrojs/react to version 4.3.0 in package.json and package-lock.json.
    - Added engine requirement for Node.js version 18 or higher.
    - Removed outdated and unused Vercel output files to streamline the project structure.
    - Adjusted Vercel configuration for improved routing and filesystem handling.

--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/api/auth/register.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../../../chunks/register_BGjxXnZA.mjs';
-export { renderers } from '../../../renderers.mjs';
-
-const page = () => _page;
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/api/body_parts/_body_part_id_/muscle_tests.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../../../../chunks/muscle_tests_CiNG8f-Y.mjs';
-export { renderers } from '../../../../renderers.mjs';
-
-const page = () => _page;
-
-export { page };
